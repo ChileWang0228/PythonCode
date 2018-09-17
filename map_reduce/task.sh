711eb840f0c31f6abeecdbf65880ff4d67a8fd9b
ChileWang0228 <912427166@qq.com>
2018-09-17 16:27:45 +0800

    upload the code

---
+++ map_reduce/task.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#sudo su
+hadoop fs -rm -r gutenberg-output
+cd /usr/local/hadoop/hadoop-3.0.3
+bin/hadoop jar share/hadoop/tools/lib/hadoop-streaming-3.0.3.jar \
+-file /home/map_reduce/mapper.py    -mapper /home/map_reduce/mapper.py \
+-file /home/map_reduce/reducer.py   -reducer /home/map_reduce/reducer.py \
+-input gutenberg/* -output gutenberg-output
+mkdir -p /home/map_reduce_result
+cd /home/map_reduce_result
+hadoop fs -get gutenberg-output
+cur_date="`date +%Y%m%d_%H:%M:%S`" 
+name="test"
+mv gutenberg-output $cur_date"_"$name
